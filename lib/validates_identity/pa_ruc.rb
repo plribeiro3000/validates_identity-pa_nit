757e2fe422857576bb4a2ed047086916df7d45d4
Paulo Ribeiro <plribeiro3000@gmail.com>
2024-02-27 07:28:10 -0300

    chore(*): Initial Commit

---
+++ lib/validates_identity/pa_ruc.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "pa_ruc/version"
+
+module ValidatesIdentity
+  module PaRuc
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/validates_identity/pa_ruc_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ValidatesIdentity::PaRuc do
+  it "has a version number" do
+    expect(ValidatesIdentity::PaRuc::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
